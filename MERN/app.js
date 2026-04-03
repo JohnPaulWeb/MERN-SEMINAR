9229f8a32b12bfeb40a26ff0fa5550da29310fb3
JohnPaulWeb <braganzajohnpaul@gmail.com>
2026-04-03 22:40:20 +0800

    Learning | MERN Stack

--- MERN/app.js
+++ MERN/app.js
@@ -4,9 +4,4 @@ const greet = require("./greet");
 
 console.log(math.add(2, 3));
 console.log(math.sub(5, 2));
-
-module.exports = function greet(name) {
-    console.log("Hello", name);
-}
-
-greet("John");
+console.log(greet("John"));
