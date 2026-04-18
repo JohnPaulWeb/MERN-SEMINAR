e09a6174ddc7974fd7e6c0965e9e357da0c760f6
JohnPaulWeb <braganzajohnpaul@gmail.com>
2026-04-18 15:48:29 +0800

    Debug Faulty Code

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let name = "John";
+
+const greet = (name) => {
+    return "Hello " + name;
+};
+
+console.log(greet(name));
+
+
+let userAge = 25;
+console.log(userAge);
+
+
+document.getElementById("output").innerHTML = "Debugging Successful";
